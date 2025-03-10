126a7e2298bcefee06a861fc319600187b6562a8
Hakeem <hazaak009@gmail.com>
2025-03-10 13:10:34 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl">Welcome to my gay website</h1>
+      <h1 className="text-4xl">Welcome to prince gay website</h1>
     </div>
   );
 }
